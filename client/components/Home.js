d957bbfc665501c1a4ea060d781978370d1804bc
MehdiDissem <mehdissem@gmail.com>
2023-01-02 19:29:35 +0100

    beginning with home page

--- client/components/Home.js
+++ client/components/Home.js
@@ -2,6 +2,7 @@ import {
   Text,
   VStack,
   Divider,
+  Header,
   Box,
   Input,
   Center,
@@ -12,224 +13,61 @@ import {
   Avatar,
   ScrollView,
 } from "native-base";
-import { StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Footer from "./Footer";
-export default function Home({navigation}) {
-  const [data, setData] = useState([]);
 
-  const getPosts = () => {
-    axios
-      .get("http:/192.168.1.6:5000/posts/")
-      .then((res) => {
-        setData(res.data);
-      })
-      .catch((err) => console.log(err));
-  };
-  useEffect(() => getPosts(), []);
+const imaage = {
+  uri: "https://i.ibb.co/S6BX4nQ/eberhard-grossgasteiger-j-CL98-LGaeo-E-unsplash.jpg",
+};
 
+export default function Home({ navigation }) {
   return (
-    <Box>
-      <Box>
-        <Center style={style.Header}>
-          <VStack
-            my="4"
-            space={5}
-            w="100%"
-            maxW="360px"
-            divider={
-              <Box px="2">
-                <Divider />
-              </Box>
-            }
-          >
-            <VStack w="100%" space={5} alignSelf="center">
-              <Input
-                style={style.search}
-                placeholder="Search"
-                variant="filled"
-                borderRadius="30"
-                py="1"
-                px="2"
-              />
-            </VStack>
-          </VStack>
-        </Center>
-        <Center style={style.graybox}>
-          <Box style={{ color: "black" }}>
-            <HStack>
-              <Button style={style.searchButtons}>
-                <Text>Country</Text>
-              </Button>
-              <Button style={style.searchButtons}>
-                <Text>Time</Text>
-              </Button>
-              <Button style={style.searchButtons}>
-                <Text>Role</Text>
-              </Button>
-              <Button style={style.searchButtons}>
-                <Text>Weight</Text>
-              </Button>
-            </HStack>
-          </Box>
-        </Center>
-        <ScrollView>
-          {data.map((e) => {
-            return (
-              <Box>
-                <Container>
-                  <HStack space={2}>
-                    <Center style={style.left}>
-                      <Avatar
-                        source={{
-                          uri: "https://ca.slack-edge.com/T03T17WCLPP-U03TQKNA1V2-9b6948bb8dc7-72",
-                        }}
-                      />
-                    </Center>
-                    <Container style={style.Middle}>
-                      <Heading size="sm">Med Amine Amdouni</Heading>
-                      <Text>{e.postTime}</Text>
-                      <Text>📍Ariana,Tunis</Text>
-                    </Container>
-                    <Container>
-                      <Text
-                        style={{
-                          backgroundColor:
-                            e.type === "reciver" ? "#B8E1BF" : "#EAC7CA",
-                          margin: 5,
-                        }}
-                      >
-                        {e.type}
-                      </Text>
-                      <Text
-                        style={{
-                          backgroundColor:
-                            e.type === "reciver" ? "#B8E1BF" : "#EAC7CA",
-                          margin: 5,
-                        }}
-                      >
-                        3kg
-                      </Text>
-                      <Text
-                        style={{
-                          backgroundColor:
-                            e.type === "reciver" ? "#B8E1BF" : "#EAC7CA",
-                          margin: 5,
-                        }}
-                      >
-                        Germany
-                      </Text>
-                    </Container>
-                  </HStack>
-                  <Container style={{ paddingLeft: 50 }} fontWeight="400">
-                    <Text
-                      style={{
-                        backgroundColor:
-                          e.type === "reciver" ? "#B8E1BF" : "#EAC7CA",
-                        margin: 5,
-                      }}
-                    >
-                      Departure Time:
-                    </Text>
-                    <Text
-                      style={{
-                        backgroundColor:
-                          e.type === "reciver" ? "#B8E1BF" : "#EAC7CA",
-                        margin: 5,
-                      }}
-                    >
-                      {e.departTime}
-                    </Text>
-                  </Container>
-                </Container>
-                <Divider my={2} />
-              </Box>
-            );
-          })}
-          {/* sender profile begins here */}
+    <>
+      <ScrollView>
+        <ImageBackground source={imaage}>
           <Box>
-            <Container>
-              <HStack space={2}>
-                <Center style={style.left}>
-                  <Avatar
-                    source={{
-                      uri: "https://ca.slack-edge.com/T03T17WCLPP-U03TQKNA1V2-9b6948bb8dc7-72",
-                    }}
-                  />
-                </Center>
-                <Container style={style.Middle}>
-                  <Heading size="sm">Med Amine Amdouni</Heading>
-                  <Text>2 mins ago</Text>
-                  <Text>📍Ariana,Tunis</Text>
-                </Container>
-                <Container>
-                  <Text style={{ backgroundColor: "#B8E1BF", margin: 5 }}>
-                    Sender
-                  </Text>
-                  <Text style={{ backgroundColor: "#B8E1BF", margin: 5 }}>
-                    3kg
-                  </Text>
-                  <Text style={{ backgroundColor: "#B8E1BF", margin: 5 }}>
-                    Germany
-                  </Text>
-                </Container>
-              </HStack>
-              <Container style={{ paddingLeft: 50 }} fontWeight="400">
-                <Text>Departure Time:</Text>
-                <Text>01 January 2023 at 11PM</Text>
-              </Container>
-            </Container>
-            <Divider my={2} />
+            <Box style={style.Header}>
+              <VStack>
+                <Heading style={style.logo}>DeliVair</Heading>
+              </VStack>
+            </Box>
           </Box>
-          {/* sender profile ends here */}
-          {/* shipper profile starts here */}
-          <Box backgroundColor="#F2F6F6">
-            <Container>
-              <HStack space={2}>
-                <Center style={style.left}>
-                  <Avatar
-                    source={{
-                      uri: "https://ca.slack-edge.com/T03T17WCLPP-U03TFHEL7FG-4e2c417ed04b-72",
-                    }}
-                  />
-                </Center>
-                <Container style={style.Middle}>
-                  <Heading size="sm">Aziz Sellini</Heading>
-                  <Text>2 mins ago</Text>
-                  <Text>📍Ariana,Tunis</Text>
-                </Container>
-                <Container>
-                  <Text style={{ backgroundColor: "#EAC7CA", margin: 5 }}>
-                    Shipper
-                  </Text>
-                  <Text style={{ backgroundColor: "#EAC7CA", margin: 5 }}>
-                    8kg
-                  </Text>
-                  <Text style={{ backgroundColor: "#EAC7CA", margin: 5 }}>
-                    Sweden
-                  </Text>
-                </Container>
-              </HStack>
-              <Container style={{ paddingLeft: 50 }} fontWeight="400">
-                <Text>Departure Time:</Text>
-                <Text>01 January 2023 at 11PM</Text>
-              </Container>
-            </Container>
-          </Box>
-          {/* shipper profile ends here */}
-          <Divider my={2} />
-        </ScrollView>
-      </Box>
-      <Footer navigation={navigation} />
-    </Box>
+        </ImageBackground>
+        <Box style={style.inputBox}>
+          <HStack >
+          <Container >
+            <Button style={style.role}>Role</Button>
+          </Container>
+          <Container>
+            <Button style={style.role}>Weight</Button>
+          </Container>
+          <Container>
+            <Button style={style.role}>Country</Button>
+          </Container>
+          </HStack>
+        </Box>
+        <Box>
+          <Container>
+            <Text>Hello</Text>
+          </Container>
+        </Box>
+      </ScrollView>
+    </>
   );
 }
 
 const style = StyleSheet.create({
   Header: {
-    backgroundColor: "#EBC8CB",
-    paddingTop: 30,
+    // backgroundColor: "#EBC8CB",
+    paddingTop: 80,
+    width: 500,
+    height: 379,
+    left: 0,
+    top: -33,
+    ImageBackground:
+      "https://i.ibb.co/S6BX4nQ/eberhard-grossgasteiger-j-CL98-LGaeo-E-unsplash.jpg",
   },
   search: {
     width: 100,
@@ -250,5 +88,28 @@ const style = StyleSheet.create({
   left: {
     width: 50,
     margin: 10,
+  },
+  logo: {
+    width: 143,
+    height: 48,
+    left: 12,
+    top: 10,
+    fontSize: 30,
+  },
+  inputBox: {
+    width: 328,
+    height: 466,
+    left: 45,
+    backgroundColor: "white",
+    borderRadius: 30,
+    marginTop: -230,
+  },
+  role: {
+    width: 60,
+    height: 35,
+    left: 52,
+    top: 30,
+    backgroundColor:"#E7C7C8",
+    borderRadius:14
   },
 });
