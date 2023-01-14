66886bc42be2041c50f7a62ead4c596f7a58ab86
amineamdouni <amineamdouni24@gmail.com>
2023-01-14 16:53:30 +0100

    done with product/user/post controller and routes

---
+++ server/routes/notification.routes.js
@@ -0,0 +1,3 @@
+var router = require("express").Router();
+const {} = require("../controller/notification.controller");
+module.exports = router;
--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,27 +1,24 @@
 var router = require("express").Router();
 const {
-  get,
+  getAll,
   add,
   verify,
-  getOne,
-  deleteFriend,
-  updateFriend,
-  getfriends,
-
+  getOneByemail,
+  deleteAccount,
+  updateFriends,
   getOneById,
-
-  ban
-
+  ban,
 } = require("../controller/user.controller");
-router.get("/", get);
-router.get("/contacts", getfriends);
-router.post("/", add);
-// router.put('/:id',verify)
-router.get("/:email", getOne);
-router.get("/id/:id", getOneById);
-router.delete("/:id", deleteFriend);
+router.get("/", getAll);
 
-router.put("/:id", updateFriend);
+router.post("/", add);
+router.put("/verify/:id", verify);
+router.put("/ban/:id", ban);
+router.get("/:email", getOneByemail);
+router.get("/id/:id", getOneById);
+router.delete("/:id", deleteAccount);
+
+router.put("/:id", updateFriends);
 
 
 
