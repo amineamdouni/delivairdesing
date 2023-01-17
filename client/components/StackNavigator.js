1fb535d92e68a9b2a169bcabdf7edbcf34502c02
AzizSelini <113175679+AzizSelini@users.noreply.github.com>
2023-01-17 16:29:07 +0100

    hi

--- client/components/StackNavigator.js
+++ client/components/StackNavigator.js
@@ -44,6 +44,7 @@ const Stacks = () => {
 
       
 
+        <Stack.Screen name="setting" component={UserSetting}/>
         <Stack.Screen name="login" component={Login} />
         <Stack.Screen name="signup" component={SignUp} />
         <Stack.Screen name="form" component={Form} />
@@ -57,7 +58,6 @@ const Stacks = () => {
         <Stack.Screen name="contarct" component={Alert} />
         <Stack.Screen name="home" component={Main} />
         <Stack.Screen name="messages" component={Messages} />
-  <Stack.Screen name="setting" component={UserSetting}/>
         <Stack.Screen name="track" component={Track} />
         <Stack.Screen name="profile" component={Profile} />
         <Stack.Screen name="addpost" component={AddPost} />
