15c6c1f858251d1f3109c4089e6279a7ff54ebbe
houssem <houssemfraj5@gmail.com>
2023-01-20 10:00:29 +0000

    Number of Transactions per Month done

--- admin/pages/home.tsx
+++ admin/pages/home.tsx
@@ -9,6 +9,16 @@ const data = [
   { month: 'Jun', users: 1200 },
   { month: 'Jul', users: 1500 },
 ];
+const data2 = [
+  { month: 'Jan', transactions: 500 },
+  { month: 'Feb', transactions: 400 },
+  { month: 'Mar', transactions: 300 },
+  { month: 'Apr', transactions: 350 },
+  { month: 'May', transactions: 450 },
+  { month: 'Jun', transactions: 550 },
+  { month: 'Jul', transactions: 600 },
+];
+
 
 const home = () => {
   return (
@@ -23,7 +33,19 @@ const home = () => {
        <Legend />
        <Bar dataKey="users" fill="#8884d8" />
       </BarChart>
+      
+      <h2>Number of Transactions per Month</h2>
+      <BarChart width={600} height={300} data={data2}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+       <XAxis dataKey="month" tick={{ fill: '#fff' }}  label={{ value: 'Months', position: 'insideBottomRight', fill: '#fff' }}/>
+       <YAxis tick={{ fill: '#fff' }} label={{ value: 'Transactions', angle: -90, position: 'insideLeft', fill: '#fff' }}/>
+       <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
+       <Tooltip />
+       <Legend />
+       <Bar dataKey="transactions" fill="#8884d8" />
+      </BarChart>
     </div>
+  
   )
 }
 
